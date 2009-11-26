32d41d826b3a7d8dbf8bb5053cf6a55bdd141f70
Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
2009-11-26 11:31:45 +0530

    Step2: Introduce fill_super and get a valid root inode
    Signed-off-by: Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>

--- super.c
+++ super.c
@@ -28,16 +28,98 @@
 
 #include <linux/module.h>
 #include <linux/fs.h>
+#include <linux/pagemap.h>
 #include <linux/version.h>
 #include <linux/slab.h>
+#include <linux/sched.h>
+#include "samplefs.h"
 
 /* helpful if this is different than other fs */
 #define SAMPLEFS_MAGIC     0x73616d70 /* "SAMP" */
 
-static int samplefs_fill_super(struct super_block * sb, void * data, int silent)
+static void samplefs_put_super(struct super_block *sb)
 {
-	printk(KERN_ERR "samplefs not fully done\n");
-	return -EINVAL;
+	struct samplefs_sb_info *sfs_sb;
+
+	sfs_sb = SFS_SB(sb);
+	if (sfs_sb == NULL) {
+		/* Empty superblock info passed to unmount */
+		return;
+	}
+	/* FS-FILLIN your fs specific umount logic here */
+	kfree(sfs_sb);
+	sb->s_fs_info = NULL;
+	return;
+}
+
+struct super_operations samplefs_super_ops = {
+	.statfs         = simple_statfs,
+	.drop_inode     = generic_delete_inode, /* Not needed, is the default */
+	.put_super      = samplefs_put_super,
+};
+
+struct inode *samplefs_get_inode(struct super_block *sb, int mode, dev_t dev)
+{
+	struct inode * inode = new_inode(sb);
+
+	if (inode) {
+		inode->i_mode = mode;
+		inode->i_uid = current_fsuid();
+		inode->i_gid = current_fsgid();
+		inode->i_blocks = 0;
+		inode->i_atime = inode->i_mtime = inode->i_ctime = CURRENT_TIME;
+		switch (mode & S_IFMT) {
+			default:
+				init_special_inode(inode, mode, dev);
+				break;
+			case S_IFREG:
+				printk(KERN_INFO "file inode\n");
+				inode->i_op = &simple_dir_inode_operations;
+				break;
+			case S_IFDIR:
+				inode->i_op = &simple_dir_inode_operations;
+
+				/* link == 2 (for initial ".." and "." entries) */
+				inode->i_nlink++;
+				break;
+		}
+	}
+	return inode;
+}
+
+static int samplefs_fill_super(struct super_block *sb, void *data, int silent)
+{
+	struct inode *root;
+	struct samplefs_sb_info * sfs_sb;
+
+	sb->s_maxbytes = MAX_LFS_FILESIZE; /* NB: may be too large for mem */
+	sb->s_blocksize = PAGE_CACHE_SIZE;
+	sb->s_blocksize_bits = PAGE_CACHE_SHIFT;
+	sb->s_magic = SAMPLEFS_MAGIC;
+	sb->s_op = &samplefs_super_ops;
+	sb->s_time_gran = 1; /* 1 nanosecond time granularity */
+
+	printk(KERN_INFO "samplefs: fill super\n");
+	root = samplefs_get_inode(sb, S_IFDIR | 0755, 0);
+	if (!root)
+		return -ENOMEM;
+
+	sb->s_fs_info = kzalloc(sizeof(struct samplefs_sb_info), GFP_KERNEL);
+	sfs_sb = SFS_SB(sb);
+	if (!sfs_sb) {
+		iput(root);
+		return -ENOMEM;
+	}
+	printk(KERN_INFO "samplefs: about to alloc dentry root inode\n");
+	sb->s_root = d_alloc_root(root);
+	if (!sb->s_root) {
+		iput(root);
+		kfree(sfs_sb);
+		sb->s_fs_info = NULL;
+		return -ENOMEM;
+	}
+	/* FS-FILLIN your filesystem specific mount logic/checks here */
+	return 0;
 }
 
 static struct dentry *samplefs_mount(struct file_system_type *fs_type,
@@ -49,8 +131,6 @@ static struct dentry *samplefs_mount(struct file_system_type *fs_type,
 
 static void samplefs_kill_sb(struct super_block *sb)
 {
-	kfree(sb->s_fs_info);
-	sb->s_fs_info = NULL;
 	kill_litter_super(sb);
 }
 
